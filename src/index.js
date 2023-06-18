ab1f7223a6183e1ad82c71a92838ef736e1c8eac
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2023-06-18 20:07:56 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello Word"});
+app.use(express.json());
+
+const clients = [];
+
+function verifyIfExistsClientName(request, response, next) {
+    
+}
+
+
+app.post("/client", (request, response) => {
+    const  { nome, celular, endereco, estado, indicacao } = request.body;
+
+    const clientAlreadyExists = clients.some(
+        (client) => client.nome === nome
+        );
+
+        if(clientAlreadyExists) {
+            return response.status(400).json({ error: "Cliente já existe!" });
+        }
+
+
+
+    clients.push({
+        
+        nome,
+        celular,
+        endereco,
+        estado,
+        indicacao,
+        id: uuidv4(),
+
+    });
+
+    return response.status(201).send();
 })
 
 app.listen(3333);
